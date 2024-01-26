e2a5bd66398321c94aafa9ffb841e1c1b6909476
Viacheslav <fx.slav21@gmail.com>
2024-01-26 15:54:06 +0300

    Second commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, request
 from model.post import Post
 
 posts = []
+comments = []
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ app = Flask(__name__)
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Post):
-            return {'body': obj.body, 'author': obj.author, 'comment': obj.comment}
+            return {'id': obj.id, 'body': obj.body, 'author': obj.author}
         else:
             return super().default(obj)
 
@@ -22,10 +23,15 @@ def read_posts():
     return jsonify({'posts': posts})
 
 
+# @app.route('/posts/<post_id>', methods=['GET', 'DELETE'])
+# def show_post(post_id):
+#     return jsonify({'posts': posts['id']})
+
+
 @app.route('/post', methods=['POST'])
 def create_post():
     post_json = request.get_json()
-    post = Post(post_json['body'], post_json['author'])
+    post = Post(post_json['id'], post_json['body'], post_json['author'])
 
     posts.append(json.loads(json.dumps(post, cls=CustomJSONEncoder)))
     return jsonify({'status': 'success'})
--- model/post.py
+++ model/post.py
@@ -1,11 +1,9 @@
 from model.user import User
-from model.comment import Comment
 
 
 class Post:
 
-    def __init__(self, id: int, body: str, author: User, comments: list):
+    def __init__(self, id: int, body: str, author: User):
         self.id = id
         self.body = body
         self.author = author
-        self.comments = comments
